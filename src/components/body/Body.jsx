488e3e76cfa3585daf996b7621141639d8f0e383
sdf-nf <sadafmokhtaroghli@gmail.com>
2022-06-07 23:29:44 +0430

    table in body component is almost done

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
-import { HeaderText } from "../../utils/Utility";
+import YellowBgButton, { HeaderText } from "../../utils/Utility";
 import "./../../assets/styles/Body.css";
 import { CategoryScale } from "chart.js";
 import Chart from "chart.js/auto";
@@ -21,6 +21,27 @@ const state = {
   ],
 };
 
+const tableData = [
+  {
+    expireDate: "۱۴ شهریور ۱۴۰۱",
+    name: "بسته اینترنت ۳ماهه",
+    discount: "20%",
+    price: "130,000",
+  },
+  {
+    expireDate: "۱۴ شهریور ۱۴۰۱",
+    name: "بسته اینترنت ۳ماهه",
+    discount: "20%",
+    price: "130,000",
+  },
+  {
+    expireDate: "۱۴ شهریور ۱۴۰۱",
+    name: "بسته اینترنت ۳ماهه",
+    discount: "20%",
+    price: "130,000",
+  },
+];
+
 export const Body = () => {
   return (
     <div>
@@ -35,7 +56,31 @@ export const Body = () => {
           }}
         />
       </section>
-      <section></section>
+      <section className="table-container">
+        <HeaderText text={"بسته های پیشنهادی مخصوص خود شما!"} />
+        <table dir="rtl">
+          <tr>
+            <th>تاریخ انقضا بسته</th>
+            <th>نام بسته</th>
+            <th>تخفیف</th>
+            <th>قیمت</th>
+            <th></th>
+          </tr>
+          {tableData.map((specialOffer, index) => {
+            return (
+              <tr className={index === tableData.length - 1 ? "no-border" : ""}>
+                <td>{specialOffer.expireDate}</td>
+                <td>{specialOffer.name}</td>
+                <td>{specialOffer.discount}</td>
+                <td>{specialOffer.price}</td>
+                <td>
+                  <YellowBgButton text={"خرید"} />
+                </td>
+              </tr>
+            );
+          })}
+        </table>
+      </section>
     </div>
   );
 };
